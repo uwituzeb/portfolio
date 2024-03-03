f1f09e7eb43075f4a9a688ace7cb5e27734d3f6e
bernice uwituze <133651805+uwituzeb@users.noreply.github.com>
2024-03-03 10:54:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.hamburger-icon');
-    menu.classList.toggle('open');
-    icon.classList.toggle('open');
-}
+const hamburgerButton = document.querySelector('.hamburger-btn');
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const navLinks = document.querySelector(".nav-links");
+
+hamburgerButton.addEventListener('click', () => {
+
+// target hamburger button to display navlinks when clicked and turn the button into x-mark
+hamburgerBtn.addEventListener('click', () => {
+    if (navLinks.style.display === 'none' || navLinks.style.display === '') {
+        navLinks.style.display = 'block';
+        hamburgerButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        hamburgerBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    } else {
+        navLinks.style.display = 'none';
+        hamburgerButton.innerHTML = '☰';
+        hamburgerBtn.innerHTML = '☰';
+    }
